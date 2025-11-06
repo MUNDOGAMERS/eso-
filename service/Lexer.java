440e0f0998d2b15c2207b3e90832b6659baaa49b
Blesware <juanfelipezoghbirobles20@gmail.com>
2025-11-06 13:01:08 -0500

    Arreglos menores

--- service/Lexer.java
+++ service/Lexer.java
@@ -3,57 +3,71 @@ import model.*;
 import java.util.*;
 
 public class Lexer {    
+
     final String numbers[] = {"0","1","2","3","4","5","6","7","8","9"};
-
-    final String identifiers [] = {"a", "b", "c", "d", "e", 
-                                    "f", "g", "h", "i", "j", "k", "l", "m", "n", 
-                                    "ñ", "o", "p", "q", "r", "s", "t", "u", "v", 
-                                    "w", "x", "y", "z"};
-
     final String operators [] = {"+","-","/","*"};
     final String delimiters [] = {";","(",")"};
+    final String identifiers [] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+                                   "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
     public boolean filterChar(String caracter){
+
         boolean x = true;
         int i = 0;
+
         while(x != false){
+
             if(caracter.equalsIgnoreCase(numbers[i])){
+
                 x = false;
                 return true;
             }
+
             i++;
         }
+
         i=0;
         x=true;
+
         while(x != false){
+
             if(caracter.equalsIgnoreCase(identifiers[i])){
+
                 x = false;
                 return true;
             }
+
             i++;
         }
 
         i=0;
         x=true;
+
         while(x != false){
+
             if(caracter.equalsIgnoreCase(operators[i])){
+
                 x = false;
                 return true;
             }
+
             i++;
         }
 
         i=0;
         x=true;
+
         while(x != false){
+
             if(caracter.equalsIgnoreCase(delimiters[i])){
+
                 x = false;
                 return true;
             }
+
             i++;
         }
-
-
+        
         return false;
     }
 
